786638ab29c27bfae32b3f3fb6ef4d6912cdff85
AhmedMahfouz <ahmelsamahy@gmail.com>
2023-02-14 20:42:44 +0100

    feat<main.js>: add function to add new row

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+var selectValue = [];
+
 function getInputForSelect() {
-  var mainContainer = document.querySelectorAll("#selectInputContainer [type='text']");
-  var selectValue = [];
-  for (var i = 0; i < mainContainer.length; i++) {
-     selectValue.push(mainContainer[i].value)
+  var inputData = document.querySelectorAll("#selectInputContainer [type='text']");
+  for (var i = 0; i < inputData.length; i++) {
+    if (inputData[i].value.length >0) {
+      selectValue.push(inputData[i].value)
+    }
   }
-  console.log(selectValue[1]);
+  console.log(selectValue);
+  return false;
 }
+
+var i = 0;
+var original = document.getElementsByClassName("tableRow")[0];
+
+function duplicate() {
+  var clone = original.cloneNode(true); // "deep" clone
+  clone.id = "";
+  // or clone.id = ""; if the divs don't need an ID
+  original.parentNode.appendChild(clone);
+}
+
