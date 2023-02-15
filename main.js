5082f7e1827f2f01162d8fd3e2ceb75babc033e2
AhmedMahfouz <ahmelsamahy@gmail.com>
2023-02-15 01:17:42 +0100

    feat<index.html, main.js>: generate the option in list from user input

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 var selectValue = []; // to store the input from user, that will be used in making the values in select list
 
+// called after clicking the "Update value" button to add the user data in the list of select
 function getInputForSelect() {
+  selectValue = []
   var inputData = document.querySelectorAll("#selectInputContainer [type='text']");
   for (var i = 0; i < inputData.length; i++) {
     if (inputData[i].value.length > 0) {
-      selectValue.push(inputData[i].value)
+      selectValue.push(String(inputData[i].value))
     }
   }
   console.log(selectValue);
   return false;
 }
 
-var i = 0;
-var original = document.getElementById("tableRow");
-var GTB = document.getElementById("generateTableButton");
+function addSelectChild() {
+  var selectParent = document.getElementById("userPreEnteredValue");
+  selectParent.innerHTML = "";
+  var html = "";
+  for (var i = 0; i < selectValue.length; i++) {
+    html += "<option value=" + selectValue[i] + ">" + selectValue[i] + "</option>" + "<br>";
+  }
 
+  selectParent.innerHTML = html;
+}
 
 function duplicate() {
   var clone = original.cloneNode(true);
